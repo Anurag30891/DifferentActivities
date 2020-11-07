d154d6159ca7db8e9c068dfa4b094962f1a7e944
ProfessionalwithStandards <64947812+ProfessionalwithStandards@users.noreply.github.com>
2020-11-07 12:18:36 +0530

    Fork Test

--- app/src/main/java/android/example/differentactivities/LifeCycleActivity.java
+++ app/src/main/java/android/example/differentactivities/LifeCycleActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 public class LifeCycleActivity extends AppCompatActivity {
 
     // This method is for onCreate
+    // Second Test for Fork
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
